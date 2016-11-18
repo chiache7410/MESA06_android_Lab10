6cfbe9f156d463e25c72dd2aec1da88dfdf37175
chiache <chiache7410@gmail.com>
2016-11-18 16:35:55 +0800

    2016-11-18_save & read

--- app/src/main/java/tw/org/iii/lab10/MainActivity.java
+++ app/src/main/java/tw/org/iii/lab10/MainActivity.java
@@ -1,13 +1,38 @@
 package tw.org.iii.lab10;
 
+import android.content.SharedPreferences;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SharedPreferences sp;
+    private SharedPreferences.Editor editor;
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView)findViewById(R.id.tv);
+        sp = getSharedPreferences("gamedata", MODE_PRIVATE);
+        editor = sp.edit();
+    }
+    //偏好設定 => save
+    public void test1(View v){
+        editor.putInt("stage", 3);
+        editor.putString("user", "Brad");
+        editor.commit();
+        Toast.makeText(this, "Save OK", Toast.LENGTH_SHORT).show();
+    }
+    //偏好設定 => read
+    public void test2(View v){
+        int stage = sp.getInt("stage", 0);
+        String name = sp.getString("user","nobody");
+        tv.setText("Stage: " + stage + "\n" + "User: " + name);
     }
 }
